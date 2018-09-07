b8bb982a45d0de59dd07e1ede9f981aaceb4c621
Marlen <marlen.gonzalez.dse@uteq.edu.mx>
2018-09-07 12:11:43 -0500

    division y llamada a division

--- Archivo_1.c
+++ Archivo_1.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define RESULTADO 5
+
+float division(float a, float b);
 int haz_una_suma(int a, int b);
 
 int main()
 {
 	printf("Hello world \n");
+	int div = division();
 	return 0;
 }
